72994867d23f94521109674eed6c3bd36a32155d
AdeyemiTestimony <90443750+AdeyemiTestimony@users.noreply.github.com>
2024-03-29 13:17:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 import joblib
 
 
